ae8adde14893171e7963b57a0d944392ac60a85e
Stephanie Shea <stephcshea@gmail.com>
2018-05-04 11:38:52 +0000

    created pseudo code

--- recover/recover.c
+++ recover/recover.c
@@ -20,11 +20,14 @@ int main(int argc, char *argv[])
         return 2;
     }
 
+    //counter for filename creation
     int counter = 0;
     char *buffer[512];
 
+//while !EOF
+
     //read into first 512 bytes of data
-    fread (buffer, sizeof(buffer), 1, infile)
+    fread (&buffer, sizeof(buffer), 1, infile);
     // fread (buffer, 1, 512, infile)
 
     //check if first 4 bytes correspond to jpeg data
@@ -35,16 +38,19 @@ int main(int argc, char *argv[])
         (buffer[3] & 0xf0) == 0xe0)
 
         {
+
+            //if old file exists, close old file
+
             //save with file name, start at 000 and increment so need count, type jpg
             sprintf(filename, "%03i.jpg", 2);
 
             //open file to give write privelege
             FILE *img = fopen(filename, "w");
 
-            // fwrite(data, size, number, outptr)
+            fwrite(buffer, sizeof(buffer),1 outptr)
             //fwrite(pointer to struc that contains bytes reading from,
             // size,number, new image FILE *
-
+//check for EOF
         }
     else if
         {
