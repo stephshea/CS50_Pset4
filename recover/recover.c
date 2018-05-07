4a2b0e2a9398922b8d5cacc41602db95c779098f
Stephanie Shea <stephcshea@gmail.com>
2018-05-07 01:20:27 +0000

    refined comments

--- recover/recover.c
+++ recover/recover.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
+
 typedef uint8_t  BYTE;
 
 int main(int argc, char *argv[])
@@ -23,20 +24,17 @@ int main(int argc, char *argv[])
         return 2;
     }
 
-    //need?
+    //set outptr to NULL
     FILE *outptr = NULL;
 
-    //define jpg filename array
+    //define jpg filename array with a length of 8 for filename and null char
     char filename[8];
 
     //counter for filename creation
     int counter = 0;
 
+    //size of buffer per size of jpg
     BYTE buffer[512];
-
-    // while(!EOF)
-    // while (counter != 50)
-
 
     //read into 512 bytes of data
     while (fread(&buffer, sizeof(buffer), 1, inptr) == 1)
@@ -49,26 +47,23 @@ int main(int argc, char *argv[])
             (buffer[3] & 0xf0) == 0xe0)
 
         {
-            //if old file exists, close old file
-
-            //save with file name, start at 000 and increment so need count, type jpg
+            //sprintf returns the total number of characters written to ptr
+            //the 0 flag indicates leading zeroes, the 3 indicates their shall be at most 3 leading zeroes
+            //and the i indicates it should format the variable as an int
             sprintf(filename, "%03i.jpg", counter);
 
             //open file to give write privelege
-            // FILE *img = fopen(filename, "w");
             outptr = fopen(filename, "w");
 
+            //fwrite(pointer to struc that contains bytes reading from, size,number, new image FILE *
             fwrite(buffer, sizeof(buffer), 1, outptr);
             counter++;
-
-            //fwrite(pointer to struc that contains bytes reading from,
-            //size,number, new image FILE *
-            //check for EOF
         }
 
+        //if outptr contains a jpg
         else if (outptr != NULL)
-
         {
+            //write the file
             fwrite(&buffer, sizeof(buffer), 1, outptr);
         }
     }
